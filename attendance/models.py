2f292176b4262456a32cfb1750fb5a032feb759f
Temhithorphe <Tehmhithorphe@gmail.com>
2023-12-14 21:01:36 +0100

    Major model reshapping

--- attendance/models.py
+++ attendance/models.py
@@ -1,42 +1,34 @@
 # models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
-
 
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    # date_of_birth = models.DateField()
     student_id = models.CharField(max_length=10, unique=True)
     fingerprint_id = models.CharField(max_length=10, unique=True, default=None)
-    rfid_id = models.CharField(max_length=10, unique=True)
+    rfid_id = models.CharField(max_length=12, unique=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
+
+
+class Lecturer(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.CharField(max_length=12, unique=True)
+
+
 class Attendance(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    date = models.DateField()
-    status = models.CharField(max_length=10)
+    course_title = models.CharField(max_length=50, blank=False)
+    course_code = models.CharField(max_length=7, blank=False)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT, blank=False)
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.PROTECT, blank=False)
+    date = models.DateTimeField(auto_now_add=True, blank=False)
+    status = models.CharField(choices={'lecture': 'Lecture', 'exam': 'Exam', 'test': 'Test'}, max_length=10, blank=False)
+
+
 
     def __str__(self):
-        return f'{self.student.first_name} {self.student.last_name} - {self.date}'
+        return f'{self.course_code}_{self.student.first_name}_{self.student.last_name}_{self.date}'
 
-
-
-class Lecturer(AbstractUser):
-    lecturer_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100)
-
-     # Specify unique related names for groups and user_permissions
-    groups = models.ManyToManyField(Group, related_name='lecturer_groups')
-    user_permissions = models.ManyToManyField(Permission, related_name='lecturer_permissions')
-
-
-    class Meta:
-        verbose_name = 'Lecturer'
-        verbose_name_plural = 'Lecturers'
-
-    def __str__(self):
-        return self.name
