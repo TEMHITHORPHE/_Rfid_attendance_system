2aab6e40c979aededb14ba070f40c95eeb71b992
Temhithorphe <Tehmhithorphe@gmail.com>
2023-12-14 03:39:37 +0100

    register models in admin dashboard

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Student, Attendance
+
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Attendance)
+
+
+# Register other models as needed
