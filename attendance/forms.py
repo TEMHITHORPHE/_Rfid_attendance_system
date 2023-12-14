2a176116eef137bf514ffc4a486398af69db3fc9
Temhithorphe <Tehmhithorphe@gmail.com>
2023-12-14 03:24:19 +0100

    Removed student DOB field

--- attendance/forms.py
+++ attendance/forms.py
@@ -5,4 +5,4 @@ from .models import Student
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'date_of_birth', 'student_id', 'fingerprint_id', 'rfid_id']
+        fields = ['first_name', 'last_name', 'student_id', 'fingerprint_id', 'rfid_id']
--- attendance/models.py
+++ attendance/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    date_of_birth = models.DateField()
+    # date_of_birth = models.DateField()
     student_id = models.CharField(max_length=10, unique=True)
     fingerprint_id = models.CharField(max_length=10, unique=True, default=None)
     rfid_id = models.CharField(max_length=10, unique=True)
