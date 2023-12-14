d2bcc77b2e6a3e2d65dea2b499deb011ab1183fd
Temhithorphe <Tehmhithorphe@gmail.com>
2023-12-14 04:29:25 +0100

    Add Lecturer Entity view

--- attendance/urls.py
+++ attendance/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
  
 from django.urls import path
 from . import views
-from .views import validate_rfid, enroll_rfid, set_rfid
+# from .views import validate_rfid, enroll_rfid, set_rfid, 
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -19,8 +19,9 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('mark_attendance/<str:method>/', views.mark_attendance, name='mark_attendance'),
     path('attendance_history/', views.attendance_history, name='attendance_history'),
-    path('enroll_rfid/', enroll_rfid, name='enroll_rfid'),
-    path('validate_rfid/', validate_rfid, name='validate_rfid'),
-    path('set/rfid/<int:access_code>/<str:tag_id>', set_rfid, name='set_rfid'),
+    path('enroll_rfid/', views.enroll_rfid, name='enroll_rfid'),
+    path('validate_rfid/', views.validate_rfid, name='validate_rfid'),
+    path('set/rfid/<int:access_code>/<str:tag_id>', views.set_rfid, name='set_rfid'),
+    path('lecturer/login/', views.lecturer_login, name='lecturer_login'),
 
 ]
